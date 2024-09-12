33c8ad80b154dd803c09d4ec7b6a9eefb988318d
Anthony Fu <github@antfu.me>
2024-09-12 14:56:00 +0200

    chore: update lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,13 @@
 import antfu from '@antfu/eslint-config'
 import nuxt from './.nuxt/eslint.config.mjs'
 
-export default nuxt(
-  antfu(
+export default nuxt()
+  .append(antfu(
     {
       unocss: true,
       formatters: true,
     },
-  ),
-)
+  ))
   .prepend({
     ignores: ['yak-map-pos.json'],
   })
